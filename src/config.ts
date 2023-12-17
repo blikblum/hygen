1f02da7c99e2ed127ca21ff23d908a68808ffe35
Luiz Américo Pereira Câmara <blikblum@users.noreply.github.com>
2023-12-17 20:17:07 -0300

    Add support for hygen.cjs

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,14 @@ const reversePathsToWalk = ({ folder, path }) => {
 }
 
 const configLookup = (file: string, folder: string, path: any = importedPath) =>
-  uniq(reversePathsToWalk({ folder, path }).map((p) => path.join(p, file)))
+  uniq(
+    reversePathsToWalk({ folder, path })
+      .map((p) => [
+        path.join(p, file),
+        path.join(p, file).replace(/.js$/, '.cjs'),
+      ])
+      .flat(),
+  )
 
 class ConfigResolver {
   configFile: string
